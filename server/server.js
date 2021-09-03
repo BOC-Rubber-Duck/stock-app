f3c4b0a328b4faab9b163f5cd6f9151aef79f713
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-09-03 15:46:49 -0500

    React App serving and build scripts working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
+const pathname = path.join(__dirname, '..', 'client', 'dist');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.end('request recieved by server:');
-});
+app.use(express.static(pathname));
+
+// app.get('/', (req, res) => {
+//   res.status(200);
+//   res.end('request recieved by server:');
+// });
 
 module.exports = app;
