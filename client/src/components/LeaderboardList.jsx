b68d8ad48f58bd8f91614502f2b4acfe00e9b9d5
Bill Roth <minongcopper@gmail.com>
2021-09-09 12:38:47 -0400

    axios call to api written

--- client/src/components/LeaderboardList.jsx
+++ client/src/components/LeaderboardList.jsx
@@ -1,18 +1,33 @@
 import React, {useState} from 'react';
 import './Leaderboard.css';
+import LeaderboardListElement from './LeaderboardListElement.jsx';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import axios from 'axios';
 
 const LeaderboardList = (props) => {
   // Declare a new state variable, which we'll call "count"
-  const [totalUsers, setTotalUsers] = useState(0);
-  const [friends, setFriends] = useState([]);
+  const [list, setList] = useState([]);
 
+
+  const fetchList = (user = null, page = 1) => {
+    // Make a request for a user with a given ID
+    axios.get(`${process.env.DB}/leaderboard?user=${user}page=${page}`)
+      .then(function(response) {
+        // handle success
+        console.log(response);
+        setList(list.concat(response));
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  };
 
   return (
     <div>
       <InfiniteScroll
-        dataLength={items.length}
-        next={fetchData}
+        dataLength={list.length}
+        next={fetchList}
         hasMore={true}
         loader={<h4>Loading...</h4>}
         endMessage={
@@ -21,20 +36,27 @@ const LeaderboardList = (props) => {
           </p>
         }
         // below props only if you need pull down functionality
-        refreshFunction={this.refresh}
+        /* refreshFunction={this.refresh}
         pullDownToRefresh
         pullDownToRefreshThreshold={50}
         pullDownToRefreshContent={
-          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+          <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
         }
         releaseToRefreshContent={
-          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-        }
+          <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
+        } */
       >
-        {items}
+        {list.map((element, index) => (
+          <LeaderboardListElement
+            key={element.username}
+            rank={(index + 1)}
+            username={element.username}
+            balance={element.balance}
+            friend={element.friend}/>
+        ))}
       </InfiniteScroll>
     </div>
   );
-}
+};
 
 export default LeaderboardList;
