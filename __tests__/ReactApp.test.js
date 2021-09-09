348333f4646861c9df15c8bb2181efe02948a838
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-09-08 23:53:57 -0500

    added login test

--- __tests__/ReactApp.test.js
+++ __tests__/ReactApp.test.js
@@ -10,6 +10,7 @@ configure({adapter: new Adapter()});
 import { render, cleanup } from '@testing-library/react';
 import App from '../client/src/components/App.jsx';
 import Navbar from '../client/src/components/Navbar.jsx';
+import Login from '../client/src/components/Login.jsx';
 import Trade from '../client/src/components/Trade.jsx';
 import Leaderboard from '../client/src/components/Leaderboard.jsx';
 
@@ -23,6 +24,7 @@ afterEach(() => {
 
 test('Top level App components render', () => {
   expect(shallow(<Navbar />).is('.navbar-container')).toBe(true);
+  expect(shallow(<Login />).is('.login-container')).toBe(true);
   expect(shallow(<Trade />).is('.trade-container')).toBe(true);
   expect(shallow(<Leaderboard />).is('.leaderboard-container')).toBe(true);
 });
