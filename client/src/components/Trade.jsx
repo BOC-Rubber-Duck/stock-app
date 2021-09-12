2a7521028eec2f2d4c85fedbfb00356e0765a95e
Tyler Brinkman <tbrinkman3@gmail.com>
2021-09-12 17:06:08 -0500

    Moved searchbar to new location

--- client/src/components/Trade.jsx
+++ client/src/components/Trade.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import Searchbar from './Searchbar.jsx'
-
 const Trade = (props) => {
   return (
     <div className="trade-container" id="trade-container">
-      <Searchbar />
       Buy/Sell Stocks!
     </div>
   );
