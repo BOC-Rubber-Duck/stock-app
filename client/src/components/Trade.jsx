b03078444181b5e198b9b52b561494ad606679f7
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-09-13 02:37:40 -0500

    WIP shares to buy/sell

--- client/src/components/Trade.jsx
+++ client/src/components/Trade.jsx
@@ -4,17 +4,46 @@ class Trade extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      shares: 0,
+      action: 'buy'
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
-    const { user, stockSelected } = props;
+    const { user, stockSelected } = this.props;
     return (
       <div className="trade-container" id="trade-container">
         Buy/Sell Stocks!
         <div className="trade-header" id="trade-header">
+        </div>
+        <div className="action-title" id="action-title">
 
+        </div>
+        <div className="trade-info" id="trade-info">
+          <div id="shares">
+            <label>
+            Shares to {this.state.action}
+              <input
+                name="shares"
+                type="number"
+                value={this.state.shares}
+                onChange={this.handleInputChange} />
+            </label>
+          </div>
+          <div className="trade-action" id="trade-action">
+
+          </div>
         </div>
 
       </div>
