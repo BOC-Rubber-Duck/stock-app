eeb6f7b5ca334206c5d21a82680b61be0e6ef161
Sam Brown <baudot@gmail.com>
2021-09-15 19:13:40 -0700

    Var names in env vars standardized.

--- server/db/queries.js
+++ server/db/queries.js
@@ -5,11 +5,11 @@ const saltRounds = 12;
 const starting_cash = 1000000;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 class Db {
