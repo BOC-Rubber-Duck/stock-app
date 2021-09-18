9b6a8a0a838df8139f2f07def4c1e88ac80e40dd
Margaret Kearns <margokearns@gmail.com>
2021-09-18 11:11:10 -0400

    portfolio renders from getPorfolioData

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -4,26 +4,96 @@ import Usercard from './Usercard.jsx';
 import {Link} from 'react-router-dom';
 import getPortfolioData from './helpers/getPortfolioData.js';
 
-const Portfolio = (props) => {
-  var expandedUser = getPortfolioData(props.user);
-  console.log(expandedUser);
-  const stocks = expandedUser.userPortfolio;
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        cashBalance: "0",
+        username: "",
+        portfolioValue: 0,
+        userPortfolio: [{
+          amount: 0,
+          stockName: "",
+          ticker_symbol: "",
+          valueOwned: 0,
+        }]
+      }
+    };
+  }
 
-  const stockbars = stocks.map((stockObject) => {
+  componentDidMount() {
+    var userCopy = this.props.user;
+    getPortfolioData(userCopy).then((expandedUser) => {
+      this.setState({
+        'user': expandedUser
+      });
+    }).catch((err) => {
+      console.log('error fetching addtl portfolio data', err);
+    });
+  }
+
+  render() {
+    const stocks = this.state.user.userPortfolio;
+
+    const stockbars = stocks.map((stockObject) => {
+      return (
+        <Link to="/stock-detail-page">
+          <Stockbar stock={stockObject} useCase='portfolio' onClick={this.props.onStockClick}/>
+        </Link>
+      );
+    });
+
     return (
-      <Link to="/stock-detail-page">
-        <Stockbar stock={stockObject} useCase='portfolio' onClick={props.onStockClick}/>
-      </Link>
+      <div>
+        <Usercard user={this.state.user}/>
+        <div>{stockbars}</div>
+      </div>
     );
-  });
+  }
+}
 
-  return (
-    <div>
-      <Usercard user={props.user}/>
-      <div>{stockbars}</div>
-    </div>
-  );
-};
+
+// const Portfolio = (props) => {
+//   console.log('user inside Portfolio:', expandedUser);
+//   var expandedUser = {
+//     cashBalance: "1000000",
+//     email: "mark_zuckerberg@example.com",
+//     first_name: "mark",
+//     friends: [],
+//     last_name: "zuckerberg",
+//     portfolioValue: 364720000,
+//     userPortfolio: [
+//       {
+//         amount: 1000000,
+//         exchange: "nasdaq",
+//         id: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a23",
+//         stockName: "Facebook, Inc.",
+//         ticker_symbol: "fb",
+//         user_id: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13",
+//         valueOwned: 364720000,
+//       }
+//     ]
+//   };
+//   // console.log(expandedUser);
+
+//   const stocks = expandedUser.userPortfolio;
+
+//   const stockbars = stocks.map((stockObject) => {
+//     return (
+//       <Link to="/stock-detail-page">
+//         <Stockbar stock={stockObject} useCase='portfolio' onClick={props.onStockClick}/>
+//       </Link>
+//     );
+//   });
+
+//   return (
+//     <div>
+//       <Usercard user={expandedUser}/>
+//       <div>{stockbars}</div>
+//     </div>
+//   );
+// };
 
 
 export default Portfolio;
--- client/src/components/Portfolio/helpers/getPortfolioData.js
+++ client/src/components/Portfolio/helpers/getPortfolioData.js
@@ -5,25 +5,27 @@ const getPortfolioData = (user) => {
   const stockData = stocks.slice();
   user.portfolioValue = 0;
 
-  axios.all(stockData.map((stock) =>
-    axios.get('/fetchSelectedStock', {
-      params: {
-        symbol: stock.ticker_symbol
-      }
-    })
-  ))
-    .then(axios.spread((...responses) => {
-      for (var i = 0; i < responses.length; i++) {
-        stocks[i].stockName = responses[i].name;
-        stocks[i].valueOwned = stocks[i].amount * responses[i].price;
-        user.portfolioValue += stocks[i].valueOwned;
-      }
-      // ******write the porfolio value to the database for said user*******
-      return user;
-    }))
-    .catch((err) => {
-      console.log(err);
-    });
+  return new Promise((resolve, reject) => {
+    axios.all(stockData.map((stock) =>
+      axios.get('/fetchSelectedStock', {
+        params: {
+          'symbol': stock.ticker_symbol
+        }
+      })
+    ))
+      .then(axios.spread((...responses) => {
+        for (var i = 0; i < responses.length; i++) {
+          // console.log(responses[i]);
+          stocks[i].stockName = responses[i].data.name;
+          stocks[i].valueOwned = stocks[i].amount * responses[i].data.price;
+          user.portfolioValue += stocks[i].valueOwned;
+        }
+        resolve(user);
+      }))
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 export default getPortfolioData;
