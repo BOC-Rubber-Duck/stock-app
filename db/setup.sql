bfc2820590863e49d352d4fdf8375f108596c4c5
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-09-23 17:22:26 -0600

    added putCashPosition and postTransaction queries and timestamp creation for transactions

--- db/setup.sql
+++ db/setup.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS transactions (
   transaction_type smallint NOT NULL,
   amount int NOT NULL,
   strike_price int NOT NULL,
-  time_entered timestamp NOT NULL
+  time_entered timestamp NOT NULL DEFAULT now()
 );
 
 comment on column transactions.transaction_type is 'Enum: buy: 0, sell: 1';
