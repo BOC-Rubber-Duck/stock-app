6f5af21fb49ee6b528c0a50e3fc55fa467914f22
Bill Roth <minongcopper@gmail.com>
2021-09-24 13:51:57 -0400

    convert leaderboard to percentages, improve CSS

--- client/src/components/LeaderboardList.jsx
+++ client/src/components/LeaderboardList.jsx
@@ -8,10 +8,12 @@ const LeaderboardList = (props) => {
         <LeaderboardListElement
           key={element.id}
           id={element.id}
+          initialBalance={1000000}
           rank={(index + 1)}
           index={index}
           username={element.username}
-          balance={element.cash_position}
+          cashPosition={Number(element.cash_position)}
+          portfolioValue={Number(element.portfolio_value)}
           watchingUser={element.watching_user}
           addFriend={props.addFriend}/>
       ))}
