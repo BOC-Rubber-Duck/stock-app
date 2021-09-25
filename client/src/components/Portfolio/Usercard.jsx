02394c186c6912e4175118112bf1cc7981a1c7a4
Tyler Brinkman <tbrinkman3@gmail.com>
2021-09-25 07:51:27 -0500

    Updated stock bar and usercard css, fixed bug in navbar

--- client/src/components/Portfolio/Usercard.jsx
+++ client/src/components/Portfolio/Usercard.jsx
@@ -16,25 +16,25 @@ const Usercard = (props) => {
   return (
     <div className='usercard'>
       <div className='profdetail1'>
-        <span>{props.user.username}</span>
+        <span className='prof-name'>{props.user.username}</span>
         <span></span>
-        <span>Rank: {/* props.user.rank*/}</span>
+        <span className='prof-rank'>Rank: {/* props.user.rank*/}</span>
       </div>
       <div className='profdetail1'>
-        <span>
-          <p>Cash Balance</p>
-          <p>${props.user.cashBalance}</p>
+        <span className='prof-cash-container'>
+          <p className='prof-cash-title'>Cash Balance</p>
+          <p className='prof-cash-val'>${props.user.cashBalance}</p>
         </span>
         <span></span>
-        <span>
-          <p>Stonk Value</p>
-          <p>${props.user.portfolioValue}</p>
+        <span className='prof-stonk-container'>
+          <p className='prof-stonk-title'>Stonk Value</p>
+          <p className='prof-stonk-val'>${props.user.portfolioValue}</p>
         </span>
       </div>
       <div className='profdetail2'>
         <span></span>
         <span><p className='diamond'>💎</p></span>
-        <span>
+        <span className='prof-net-container'>
           <p>Net {gainLoss}</p>
           <p>{percentChange}%</p>
         </span>
