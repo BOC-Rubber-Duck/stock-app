1083f17b31a55ce4bba6e6068c702cb0cc33b804
Tyler Brinkman <tbrinkman3@gmail.com>
2021-09-24 19:33:32 -0500

    Fixed searchbar display

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -83,19 +83,12 @@ const Searchbar = (props) => {
   return (
     <div className='searcbar-main'>
       <div className='searchbar-container'>
-        <div className='searchbar-display'>
-          <IconContext.Provider value={{className: 'searchbar-icon'}}>
-            <div className='search-icon-container'>
-              <FaSearch />
-            </div>
-          </IconContext.Provider>
           <input
             type='text'
             className='searchbar-input'
             placeholder='Search...'
             onChange={(e) => handleUserInput(e)}>
           </input>
-        </div>
       </div>
       <div className='searchbar-predictions-container'>
         {stockPredictions && <Predictions predictions={stockPredictions} predictionClick={handlePredictionClick}/>}
